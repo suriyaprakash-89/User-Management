7016ed659b1472fa10a8088ed8c6ff895f3dd14c
suriyaprakash-89 <suriyaprakash3036@gmail.com>
2025-11-10 14:02:01 +0530

    saved files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,8 +10,8 @@ function App() {
         <div className="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8 flex items-center space-x-3">
           <img src="/FSYS_logo.png" alt="Fluxion Logo" className="h-10" />
           <h1 className="w-full flex justify-center text-2xl font-semibold text-indigo-600 tracking-wide">
-  USER MANAGEMENT SYSTEM
-</h1>
+            USER MANAGEMENT SYSTEM
+          </h1>
         </div>
       </header>
 
